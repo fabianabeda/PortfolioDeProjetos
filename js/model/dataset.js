29a671ee5c9b682c37649a9c9128b2284007a507
fabianabeda <fabiana.beda@gmail.com>
2025-06-20 01:42:47 -0300

    Ajustes no layout: centralização, espaçamento e nova formação adicionada

--- js/model/dataset.js
+++ js/model/dataset.js
@@ -3,37 +3,37 @@ const dataset = [
         id: 'LicitTrack',
         image: 'img/projeto-licittrack.png',
         textodesc: 'Sistema analítico para monitoramento de licitações públicas, com backend em Node.js, frontend em React e banco de dados MongoDB. Integrado a APIs públicas do PNCP, o projeto foi registrado no INPI e hospedado via Netlify, evidenciando seu caráter técnico e inovador.',
-        endereco: '',
+        endereco: 'pages/licittrack.html',
     },
     {
         id: 'Pipeline de Dados Ambientais',
         image: 'img/desmatamento.png',
         textodesc: 'Pipeline construído com dados das APIs AQICN (qualidade do ar) e MapBiomas (uso do solo). Utilizou Airflow para orquestração, MinIO para armazenamento, Spark para processamento e PostgreSQL para carga de dados, com visualizações em Power BI.',
-        endereco: 'https://app.powerbi.com/view?r=eyJrIjoiM2UwMjc0MWMtNGM0Ni00ZjAxLWJmYjItOTBlMjE1Y2UxY2RkIiwidCI6IjVhM2UxZWI5LWM3NzctNDQ1YS04MjQyLWQ4MjVhNDYxYjEyYiJ9',
+        endereco: 'pages/pipeline.html'
     },
     {
         id: 'Nothwind',
         image: 'img/northwind.png',
         textodesc: 'Projeto acadêmico voltado ao setor comercial de uma empresa fictícia. Envolveu o uso do Power BI e Pentaho Data Integration para modelagem dimensional e criação de dashboards interativos com dados de clientes, produtos, vendedores e fornecedores.',
-        endereco: 'docs/northwind.pdf',
+        endereco: 'pages/northwind.html'
     },
     {
         id: 'Portal de Serviços',
         image: 'img/portal.png',
         textodesc: 'Sistema Portal de Serviços de Inteligência de Dados foi construído com Django no backend e tecnologias web como HTML, CSS e JavaScript no frontend. Ele integra dados estratégicos para apoiar a tomada de decisão, dispondo de módulos administrativos e de acesso para funcionários. O desenvolvimento incluiu a implementação de testes unitários e correção de bugs para assegurar a qualidade da aplicação.',
-        endereco: 'https://github.com/fabianabeda/Portal_de_Servicos_de_Inteligencia_de_Dados',
+        endereco: 'pages/portal.html',
     },
     {
         id: 'Redealogando',
         image: 'img/depressao.png',
         textodesc: 'Site desenvolvido com HTML5 e CSS3 para apoio a pessoas com depressão e ansiedade. Oferece conteúdo informativo, dicas de autocuidado, contatos de apoio psicológico e mensagens motivacionais. Interface responsiva com foco em acessibilidade, linguagem acolhedora e design pensado para transmitir conforto e empatia.',
-        endereco: 'https://fabianabeda.github.io/Redealogando/',
+        endereco: 'pages/redealogando.html',
     },
     {
         id: 'BigBurger',
         image: 'img/bigburger.png',
         textodesc: 'Aplicação Full Stack para gestão de pedidos em hamburguerias. Utilizou React com Context API e Styled Components no frontend, Node.js com Express e MongoDB no backend, autenticação com JWT e criptografia com Bcrypt. Containers Docker para ambiente isolado e integração via API REST.',
-        endereco: 'https://github.com/fabianabeda/bigburger-interface',
+        endereco: 'pages/bigburger.html',
     },
     
  
